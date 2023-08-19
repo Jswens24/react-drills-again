4bcbb309434d828b0b53593c5eabd0982e5301fa
Jessica Swenson <jessicamswens@gmail.com>
2023-08-19 14:33:33 -0600

    finished 3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [userInput, setUserInput] = useState('')
+
+  const arr = ['spaghetti', 'ice cream', 'sushi', 'bologna', 'cheese']
+
+  const list = arr.map(el => {
+    if (el.includes(userInput)) {
+      return <h4>{el}</h4>
+    }
+  })
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={e => setUserInput(e.target.value)} />
+      {list}
     </div>
   );
 }
