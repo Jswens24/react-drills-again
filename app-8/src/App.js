6d991da04be28f85e1e29ca3c60b012df6b1d9d5
Jessica Swenson <jessicamswens@gmail.com>
2023-08-19 16:48:04 -0600

    started 8

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [quote, setQuote] = useState('')
+
+  useEffect(
+    axios.get('https://api.kanye.rest')
+      .then((res) => {
+        console.log(res.data.quote)
+      })
+    , [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {quote}
     </div>
   );
 }
