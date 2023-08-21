464ff80b3d9510e678342b3b53ab3d417e6c8095
Jessica Swenson <jessicamswens@gmail.com>
2023-08-21 14:55:43 -0600

    finished 10

--- app-10/src/Details.jsx
+++ app-10/src/Details.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+
+const getDetail = (id) => {
+    return axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        .then(res => {
+            return res.data;
+        })
+}
+
+
 
 const Details = () => {
+    const [pokemon, setPokemon] = useState({})
+    let { id } = useParams();
+
+
+    useEffect(() => {
+        (async () => {
+            setPokemon(await getDetail(id))
+        })()
+    }, [])
+
+    console.log(pokemon);
+
+
     return (
-        <div>Details</div>
+        <div>
+            {pokemon.sprites && <img src={pokemon.sprites.front_shiny} alt='pokemon' />}
+            <h2>{pokemon.name}</h2>
+        </div>
     )
 }
 
--- app-10/src/Products.jsx
+++ app-10/src/Products.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { useEffect } from 'react'
 import axios from 'axios'
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const getPoke = () => {
     return axios.get('https://pokeapi.co/api/v2/pokemon/')
         .then(res => {
+            console.log(res.data.results);
             return res.data.results
         })
 }
@@ -13,6 +15,10 @@ const getPoke = () => {
 
 const Products = () => {
     const [pokemonList, setPokemonList] = useState([]);
+
+    const clickHandleDetails = (e) => {
+        console.log(e.target.id);
+    }
 
 
 
@@ -25,7 +31,11 @@ const Products = () => {
     return (
         <div>
             {pokemonList.map(poke => {
-                return <h4 key={poke.name}>{poke.name}</h4>
+                return (
+                    <Link to={`/details/${poke.name}`} key={poke.name}>
+                        <h4 id={poke.name}>{poke.name}</h4>
+                    </Link>
+                )
             })}
         </div>
     )
